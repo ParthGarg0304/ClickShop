ade1896c0fc0a9568c01c4d8ac621af76af63d80
ParthGarg0304 <parth.garg0304@gmail.com>
2024-01-24 22:33:08 +0530

    product/navbar feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Counter } from './features/counter/Counter';
 import './App.css';
-
+import ProductList from  './features/product/ProductList'
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <counter></counter>
     </div>
   );
 }
+
+
 
 export default App;
