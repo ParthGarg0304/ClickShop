248afd3fc897e2965a1977edea972fb009ddba70
ParthGarg0304 <parth.garg0304@gmail.com>
2024-01-24 23:18:35 +0530

    home final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import ProductList from  './features/product/ProductList'
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
-      <counter></counter>
+      <Home></Home>
     </div>
   );
 }
-
-
 
 export default App;
